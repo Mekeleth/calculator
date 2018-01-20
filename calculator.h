d096ad4f138877cd3d185e5c9f179c25c23352d4
Mateusz Piątkowski <garadim12@gmail.com>
2018-01-20 14:16:52 +0100

    graphical interface is ready. I have begun writing conversion algorithms

--- calculator.h
+++ calculator.h
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <string>
 #include "radio_buttons.h"
+#include "conversions.h"
 
 using namespace std;
 
@@ -26,7 +27,7 @@ protected:
     Gtk::Separator mSeparator;
     Gtk::Button mButton;
     void convertNumber();
-    void fillBuffers();
+    void fillBuffer(string);
 };
 
 Calculator::Calculator(): mButton("Convert"), topBox(Gtk::ORIENTATION_VERTICAL,Gtk::PACK_SHRINK), radioBox(Gtk::ORIENTATION_VERTICAL) {
@@ -44,8 +45,6 @@ Calculator::Calculator(): mButton("Convert"), topBox(Gtk::ORIENTATION_VERTICAL,G
     topBox.pack_start(output);
     topBox.pack_start(mSeparator);
     topBox.pack_start(mButton);
-
-
     // Show all children of the window
     show_all_children();
 }
@@ -53,23 +52,53 @@ Calculator::Calculator(): mButton("Convert"), topBox(Gtk::ORIENTATION_VERTICAL,G
 Calculator::~Calculator(){
 }
 
-void Calculator::fillBuffers(){
-
+void Calculator::fillBuffer(string text){
+    textBuffer = Gtk::TextBuffer::create();
+    textBuffer->set_text(text);
+    output.set_buffer(textBuffer);
 }
 
 void Calculator::convertNumber(){
     int button1 = buttons1.whichButton();
     int button2 = buttons2.whichButton();
-    string text = buttons1.getNumber();
+    string text = buttons1.getNumber(), formattedText;
     cout << button1 << endl << button2 << endl << text.length() << endl;
-    if(button1 = button2){
-
-    }
+    if(button1 = button2) this->fillBuffer(text);
     else if(button1 < button2){
-
+        switch(button1){
+            case 2:
+                switch(button2){
+                    case 10:
+                        formattedText = binDec(text, true);
+                    break;
+                    case 16:
+                        formattedText = binHex(text, true);
+                    break;
+                }
+            break;
+            case 10:
+                formattedText = decHex(text, true);
+            break;
+        }
+        this->fillBuffer(formattedText);
     }
     else{
-
+        switch(button1){
+            case 16:
+                switch(button2){
+                    case 10:
+                        formattedText = decHex(text, false);
+                    break;
+                    case 2:
+                        formattedText = binHex(text, false);
+                    break;
+                }
+            break;
+            case 10:
+                formattedText = binDec(text, false);
+            break;
+        }
+        this->fillBuffer(formattedText);
     }
 }
 #endif
